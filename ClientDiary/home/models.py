23cf08c87b83a905ed8677623deae94230dc7f89
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2024-07-05 13:25:28 +0530

    Update models.py

--- ClientDiary/home/models.py
+++ ClientDiary/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    client_name = models.CharField(max_length=150)
+    client_address = models.TextField()
+    client_area = models.CharField(max_length=100)
+    client_phno = models.IntegerField(max_length=10)
+    client_service_date = models.DateField()
+    client_paymentmode = models.BooleanField(default=False)
+
